6b1f3581bcc904f4ee5e3f28061b08e8754e072f
KumaravelProgrammer <kumaravelprogrammer@gmail.com>
2024-10-21 10:37:10 +0530

    Resolved merge conflicts

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import client from "prom-client";
+import { requestCountMiddleware } from "./monitering/requestCount";
+
+const app = express();
+app.use(requestCountMiddleware);
+app.use(express.json());
+
+app.get("/user", (req, res) => {
+  res.send({
+    name: "John Doe",
+    age: 25,
+  });
+});
+
+app.post("/user", (req, res) => {
+  const user = req.body;
+  res.send({
+    ...user,
+    id: 1,
+  });
+});
+
+app.get("/metrics", async (req, res) => {
+  const metrics = await client.register.metrics();
+  res.set("Content-Type", client.register.contentType);
+  res.end(metrics);
+});
+app.listen(3000);
